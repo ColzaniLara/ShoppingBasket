e92ee670cfee7691e074c8a1e585f07a7889acb6
ColzaniLa <lara.colzani@nttdata.com>
2016-03-14 00:25:04 +0100

    modificato log4j.xml per loggare su console e non su file

--- src/main/sb/purchase/Calculator.java
+++ src/main/sb/purchase/Calculator.java
@@ -17,6 +17,7 @@ public class Calculator {
 	
 		ShoppingBasket shoppingBasket= new ShoppingBasket();
 		logger.info("INPUT");
+		//System.out.println("INPUT");
 		for(int i=0; i<obj.length(); i++){
 			//logger.info("Input: "+(i+1));
 			JSONObject inputParams = obj.getJSONObject(i);
